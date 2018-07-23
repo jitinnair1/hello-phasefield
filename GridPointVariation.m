9bdfdff9598596b5ddb394f6fc08c4b308841c28
Jitin <jitin.s.nair@gmail.com>
2018-07-23 21:52:17 +0530

    minor edits

--- GridPointVariation.m
+++ GridPointVariation.m
@@ -1,10 +1,27 @@
 N=64;
-m=2;
-dt=0.01;
-for i=0.5:0.5:2.0
-    CH_FDM_GridPoint(N, m, i, dt)
+m=8;
+dt=[0.1; 0.5];
+dx=[0.4; 0.8; 1.2];
+np=numel(dx);
+istep=numel(dt);
+
+labels=strings(np, numel(dt));
+dx_by_dt=zeros(istep, 1);
+for i=1:np
+    for j=1:istep
+    CH_FDM_GridPoint(N, m, dx(i), dt(j))
+    dx_by_dt(i,j)=dx(i)/dt(j);
+    labels(i,j)="dx="+string(dx(i))+" , dt="+string(dt(j));
     hold on
+    end
 end
-title('Effect of varying dx');
-legend('dx=0.5','dx=1','dx=1.5','dx=2');
+title('Effect of varying dx and dt');
+legend(labels);
 hold off
+
+dx_by_dt(i,j)=dx_by_dt(i,j)/m;
+
+% Format Results before display
+col_labels=reshape(labels, [], 1);
+gridpoints_per_wavelength=reshape(dx_by_dt, [], 1);
+table(col_labels, gridpoints_per_wavelength)
